80496d91119252e90e6e446a1f0db4b2373b5e1b
Afarait Sulaimon Bola <afaraitsulaimon@gmail.com>
2021-03-17 13:19:15 +0100

    index Page added and separated footer and header

---
+++ utilities/footer.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<title>Title</title>
+</head>
+<body>
+
+
+
+
+            <!-- footer starts from here -->
+
+            <div class="container-fluid mt-4 bg-secondary">
+                 <div class="row justify-content-around">
+
+                      <div class="col-3">
+                         <h3>USEFUL LINKS</h3>
+                         <ul class="list-unstyled">
+                             <li><a href="" class="text-light text-decoration-none h6">About Us</a></li>
+                             <li><a href="" class="text-light text-decoration-none h6">Contact Us</a></li>
+                             <li><a href="" class="text-light text-decoration-none h6">Subscriptions</a></li>
+                             <li><a href="" class="text-light text-decoration-none h6">Register</a></li>
+                             <li><a href="" class="text-light text-decoration-none h6">Privacy Policy</a></li>
+                             <li><a href="" class="text-light text-decoration-none h6">FAQs</a></li>
+                             <li><a href="" class="text-light text-decoration-none h6">Testimonies</a></li>
+                         </ul>
+                      </div>
+
+                      <div class="col-3">
+                        <h3>FEATURED TEST</h3>
+                            <ul class="list-unstyled">
+                                <li><a href="" class="text-light text-decoration-none h6">WAEC</a></li>
+                                <li><a href="" class="text-light text-decoration-none h6">NECO</a></li>
+                                <li><a href="" class="text-light text-decoration-none h6">UTME</a></li>
+                                <li><a href="" class="text-light text-decoration-none h6">POST-JAMB</a></li>
+                            </ul>
+                      </div>
+
+
+                      <div class="col-3">
+                          <h3>Join Us on Social Media</h3>
+                          <ul class="list-unstyled">
+                                <li><a href="" class="text-light text-decoration-none h6 glyphicon glyphicon-asterisk"><span class="glyphicon glyphicon-asterisk"></span>Facebook</a></li>
+                                <li><a href="" class="text-light text-decoration-none h6">Twitter</a></li>
+                                <li><a href="" class="text-light text-decoration-none h6">Instagram</a></li>
+                            </ul>
+                      </div>
+
+                      <div class="col-3">
+                         <h3>Details</h3>
+                         <p class="text-light">Address: 108, Palm Avenue,Mushin, Lagos, Nigeria.</p>
+                         <p class="text-light">Phone: 08032716181</p>
+                         <p class="text-light">Email: info@testit.com</p>
+                      </div>
+                 </div>
+            </div>
+
+            
+            <div class="container mt-4">
+                <div class="row">
+                     <div class="col-12">
+                          <p class="text-center h4">TestIt &copy copyright <?php echo date('Y'); ?></p>
+                     </div>
+                </div>
+            </div>
+            <!-- footer ends here -->
+
+
+
+
+       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</body>
+</html>
